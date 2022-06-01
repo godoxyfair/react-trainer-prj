1ab2c25c13e69b72700bf5c62d0ad5036ccb3294
godoxyfair <godoxyfair@gmail.com>
2022-06-01 19:52:44 +0400

    Updates

---
+++ precache-manifest.b1a049443ffefae7d1b17cbb66ada048.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "882f922602d0f2a154e7",
+    "url": "/react-trainer-prj/static/css/main.51d75ebf.chunk.css"
+  },
+  {
+    "revision": "882f922602d0f2a154e7",
+    "url": "/react-trainer-prj/static/js/main.882f9226.chunk.js"
+  },
+  {
+    "revision": "6490e13bb5b74565fa25",
+    "url": "/react-trainer-prj/static/js/1.6490e13b.chunk.js"
+  },
+  {
+    "revision": "be9ced0c20547bd7d7ff",
+    "url": "/react-trainer-prj/static/js/runtime~main.be9ced0c.js"
+  },
+  {
+    "revision": "23c0af4268bee4d9204c5dd134288f81",
+    "url": "/react-trainer-prj/index.html"
+  }
+];
