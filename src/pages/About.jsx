a14cc281040479b68debef1f885bb7e6402d2915
godoxyfair <godoxyfair@gmail.com>
2022-06-21 18:40:03 +0300

    react router library 2, comment page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,11 +5,10 @@ const About = () => {
         <div style={{ width: '800px',display: 'flex' , flexWrap: 'wrap',justifyContent: 'center', height: '100vh'}}>
             <h1 style={{marginTop: '20px', color: 'white'}}>About</h1>
             <div style={{fontSize:'18pt', marginTop: '20px', lineHeight: '1.5', padding: '10px', color: 'white'}}>
-                I made this project as a React learning project. In it I used react-router-dom.
-                The application loads information into posts with JSON Placeholder, splits all loaded posts into 10 pages. Each post can be deleted and comments to it can be viewed.
-                You can also create a new post and add it to the page.
-                The application implements post sorting and search.
-                The application is decomposed into various React components.
+                I did this project while learning React. I implemented it with react-router-dom.
+                The app uploads information to the posts using JSON Placeholder, splits all the uploaded posts into 10 pages.
+                Each post can be deleted and comments on it can be viewed. You can also create a new post and add it to the page.
+                Sorting and searching is implemented in the app. The application is decomposed into different components of React.
             </div>
         </div>
 
